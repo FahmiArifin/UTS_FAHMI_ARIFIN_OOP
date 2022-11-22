406a32edfa551071bfa695a2289e6d3474d82025
FahmiArifin <118619929+FahmiArifin@users.noreply.github.com>
2022-11-22 14:32:06 +0700

    Add files via upload

---
+++ Kipas_angin.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.kipas_angin;
+
+/**
+ *
+ * @author Fahmi Arifin
+ */
+public class Kipas_angin {
+
+     public static void main(String[] args) {
+        String merk;
+        String daya;
+        String volt;
+        String tipe;
+        
+        merk ="Miyako";
+        daya="45 watt";
+        volt="220 vac";
+        tipe="kipas duduk";
+        
+        System.out.println("SPESIFIKASI KIPAS");
+        System.out.println("By Fahmi Arifin");
+        System.out.println("merk :"+merk);
+        System.out.println("cc daya :"+daya);
+        System.out.println("no volt :"+volt);
+        System.out.println("Tipe :"+tipe);
+    }
+}
